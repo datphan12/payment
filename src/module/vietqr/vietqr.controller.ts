f94de83fe0c51c3cbfbf52f764ea7df7c53a9e3c
Dat Phan <phanminhdat2409@gmail.com>
2024-10-23 22:51:50 +0700

    a

--- src/module/vietqr/vietqr.controller.ts
+++ src/module/vietqr/vietqr.controller.ts
@@ -1,16 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
   Headers,
   Post,
   UnauthorizedException,
 } from '@nestjs/common';
 import VietQRService from './vietqr.service';
 
-@Controller('vqr/api')
+@Controller('vqr')
 export default class VietQRController {
   constructor(private readonly vietQRSercive: VietQRService) {}
-  @Post('token_generate')
+  @Post('api/token_generate')
   generateToken(@Headers('authorization') authorization: string) {
     try {
       return this.vietQRSercive.generateToken(authorization);
@@ -19,11 +20,16 @@ export default class VietQRController {
     }
   }
 
-  @Post('transaction-sync')
+  @Post('api/transaction-sync')
   transactionSync(
     @Headers('authorization') authorization: string,
     @Body() body,
   ) {
     return this.vietQRSercive.transactionSync(authorization, body);
+  }
+
+  @Get('api/test')
+  test() {
+    return 'Get success';
   }
 }
